6c131980e30dc77821e77596b7e392821fc5ff3c
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-04 22:47:20 +0200

    :sparkles: Upload on homepage

---
+++ src/frontend/components/OcelUpload/OcelUpload.tsx
@@ -0,0 +1,70 @@
+import { Button, Container, Divider, Paper, Stack, Title } from "@mantine/core";
+
+import classes from "@/styles/Import.module.css";
+import {
+  useGetDefaultOcel,
+  useImportDefaultOcel,
+  useImportOcel,
+} from "@/api/fastapi/default/default";
+import { Container as ContainerIcon } from "lucide-react";
+import FileDropzone from "@/components/Dropzone/Dropzone";
+
+const OcelUpload: React.FC<{ onUpload: () => void }> = ({ onUpload }) => {
+  const { data: defaultOcels } = useGetDefaultOcel({
+    only_latest_versions: true,
+  });
+
+  const { mutate: importDefaultOcel } = useImportDefaultOcel({
+    mutation: {
+      onSuccess: onUpload,
+    },
+  });
+
+  const { mutate } = useImportOcel({
+    mutation: { onSuccess: onUpload },
+  });
+
+  return (
+
+    <Paper withBorder shadow="sm" p={22} mt={30} radius="md">
+      <FileDropzone
+        onUpload={(file) =>
+          mutate({ data: { file: file[0] }, params: { name: file[0].name } })
+        }
+        accept={[".json", ".jsonocel", ".sqlite", ".xml", ".xmlocel"]}
+        content={{
+          accept: "Drop files here",
+          idle: "Upload OCEL",
+          reject: "Not supported file format",
+          description: (
+            <>
+              Drag&apos;n&apos;drop files here to upload. We can accept only
+              <i>.sqlite, .json, .xml </i> files.
+            </>
+          ),
+        }}
+      />
+      <Stack gap={0} mt="lg">
+        <Divider />
+        {defaultOcels?.map((ocel) => (
+          <>
+            <Button
+              variant="subtle"
+              p={0}
+              onClick={() => importDefaultOcel({ params: { key: ocel.key } })}
+              leftSection={<ContainerIcon />}
+              rightSection={ocel.version}
+              justify="space-between"
+              fullWidth
+            >
+              {ocel.name}
+            </Button>
+            <Divider />
+          </>
+        ))}
+      </Stack>
+    </Paper>
+  );
+};
+
+export default OcelUpload;
--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,12 +1,16 @@
-import { useImportOcel } from "@/api/fastapi/default/default";
 import { useGetOcels, useSetCurrentOcel } from "@/api/fastapi/session/session";
-import { Container, Table, Title } from "@mantine/core";
+import OcelUpload from "@/components/OcelUpload/OcelUpload";
+import { Button, Container, Menu, Modal, Table, Title } from "@mantine/core";
 import { useQueryClient } from "@tanstack/react-query";
-import { StarIcon } from "lucide-react";
+import { EllipsisVerticalIcon, FileUp, StarIcon } from "lucide-react";
+import { useState } from "react";
 
 const Overview = () => {
   const queryClient = useQueryClient();
   const { data: ocels } = useGetOcels();
+
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+
   const { mutate: setCurrentOcel } = useSetCurrentOcel({
     mutation: {
       onSuccess: async () => {
@@ -16,43 +20,134 @@ const Overview = () => {
   });
 
   return (
-    <Container>
-      <Title>OCELS</Title>
+    <>
+      <Modal
+        opened={isUploadModalOpen}
+        onClose={() => setIsUploadModalOpen(false)}
+        size={"xl"}
+        withCloseButton={false} styles={{
+          content: {
+            backgroundColor: 'transparent', // removes background
+            boxShadow: 'none', // optional: removes drop shadow
+          },
+        }}
 
-      {ocels && (
-        <Table.ScrollContainer minWidth={800}>
-          <Table verticalSpacing="sm">
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th></Table.Th>
-                <Table.Th>Name</Table.Th>
-                <Table.Th>Uploaded At</Table.Th>
-                <Table.Th>Extensions</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {(ocels.ocels ?? []).map(
-                ({ id, name, created_at, extensions }) => (
-                  <Table.Tr
-                    key={id}
-                    onClick={() => setCurrentOcel({ params: { ocel_id: id } })}
+      >
+        <OcelUpload
+          onUpload={() => {
+            queryClient.invalidateQueries();
+            setIsUploadModalOpen(false);
+          }}
+        />
+      </Modal>
+
+      <Container>
+        <Title>OCELS</Title>
+        {ocels && ocels.ocels.length > 0 ? (
+          <Table.ScrollContainer minWidth={800}>
+            <Table verticalSpacing="sm" highlightOnHover>
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th />
+                  <Table.Th>Name</Table.Th>
+                  <Table.Th>Uploaded At</Table.Th>
+                  <Table.Th>Extensions</Table.Th>
+                  <Table.Th
+                    style={{ display: "flex", justifyContent: "end" }}
+                    px={0}
+                    align="right"
                   >
-                    <Table.Td>
-                      {id === ocels.current_ocel_id && (
-                        <StarIcon size={14} fill="blue" color="blue" />
-                      )}
-                    </Table.Td>
-                    <Table.Td>{name}</Table.Td>
-                    <Table.Td>{created_at}</Table.Td>
-                    <Table.Td>{extensions}</Table.Td>
-                  </Table.Tr>
-                ),
-              )}
-            </Table.Tbody>
-          </Table>
-        </Table.ScrollContainer>
-      )}
-    </Container>
+                    <Button
+                      variant="subtle"
+                      p={0}
+                      w={30}
+                      h={30}
+                      onClick={() => setIsUploadModalOpen(true)}
+                    >
+                      <FileUp size={20} />
+                    </Button>
+                  </Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {(ocels.ocels ?? []).map(
+                  ({ id, name, created_at, extensions }) => (
+                    <Table.Tr
+                      key={id}
+                      onClick={() =>
+                        setCurrentOcel({ params: { ocel_id: id } })
+                      }
+                    >
+                      <Table.Td>
+                        {id === ocels.current_ocel_id && (
+                          <StarIcon size={14} fill="blue" color="blue" />
+                        )}
+                      </Table.Td>
+                      <Table.Td>{name}</Table.Td>
+                      <Table.Td>{created_at}</Table.Td>
+                      <Table.Td>{extensions}</Table.Td>
+                      <Table.Td align="right" px={0}>
+                        <Menu width={200} position="left-start">
+                          <Menu.Target>
+                            <Button p={0} variant="subtle">
+                              <EllipsisVerticalIcon />
+                            </Button>
+                          </Menu.Target>
+
+                          <Menu.Dropdown>
+                            <Menu.Item>Dashboard</Menu.Item>
+
+                            <Menu.Sub>
+                              <Menu.Sub.Target>
+                                <Menu.Sub.Item>Products</Menu.Sub.Item>
+                              </Menu.Sub.Target>
+
+                              <Menu.Sub.Dropdown>
+                                <Menu.Item>All products</Menu.Item>
+                                <Menu.Item>Categories</Menu.Item>
+                                <Menu.Item>Tags</Menu.Item>
+                                <Menu.Item>Attributes</Menu.Item>
+                                <Menu.Item>Shipping classes</Menu.Item>
+                              </Menu.Sub.Dropdown>
+                            </Menu.Sub>
+
+                            <Menu.Sub>
+                              <Menu.Sub.Target>
+                                <Menu.Sub.Item>Orders</Menu.Sub.Item>
+                              </Menu.Sub.Target>
+
+                              <Menu.Sub.Dropdown>
+                                <Menu.Item>Open</Menu.Item>
+                                <Menu.Item>Completed</Menu.Item>
+                                <Menu.Item>Cancelled</Menu.Item>
+                              </Menu.Sub.Dropdown>
+                            </Menu.Sub>
+
+                            <Menu.Sub>
+                              <Menu.Sub.Target>
+                                <Menu.Sub.Item>Settings</Menu.Sub.Item>
+                              </Menu.Sub.Target>
+
+                              <Menu.Sub.Dropdown>
+                                <Menu.Item>Profile</Menu.Item>
+                                <Menu.Item>Security</Menu.Item>
+                                <Menu.Item>Notifications</Menu.Item>
+                              </Menu.Sub.Dropdown>
+                            </Menu.Sub>
+                          </Menu.Dropdown>
+                        </Menu>
+                      </Table.Td>
+                    </Table.Tr>
+                  ),
+                )}
+              </Table.Tbody>
+            </Table>
+          </Table.ScrollContainer>
+        ) : (
+          <OcelUpload onUpload={() => queryClient.invalidateQueries()} />
+        )}
+      </Container>
+    </>
   );
 };
 
