93db366cd0207dc8a17e2bd1812a127235b3c9fa
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-07 20:35:06 +0200

    :sparkles: Object graphs

--- src/frontend/plugins/ocelot/pages/objectGaph.tsx
+++ src/frontend/plugins/ocelot/pages/objectGaph.tsx
@@ -1,27 +1,75 @@
 import { RouteDefinition } from "@/plugins/types";
-import FloatingFlow from "@/plugins/ocelot/components/ObjectGraph/FloatingFlow";
-import { useO2o } from "@/api/fastapi/info/info";
+import { useO2o, useObjectAttributes } from "@/api/fastapi/info/info";
+import Graph, { NodeComponents } from "@/components/Graph";
+import { Box, Divider, Table, Text } from "@mantine/core";
+import { ObjectAttributes200Item } from "@/api/fastapi-schemas";
 import { useMemo } from "react";
-import { InputGraph } from "../components/ObjectGraph/graph";
+
+const AttributeTable: React.FC<{
+  name: string;
+  attributes?: ObjectAttributes200Item[];
+}> = ({ name, attributes }) => {
+  return (
+    <Box w="100%" bd={"1px solid black"} bg={"white"} miw={200} mih={100}>
+      <Text fw={700} size="sm" ta="center" py={"4"}>
+        {name}
+      </Text>
+      <Divider c={"black"} />
+      {attributes && attributes.length !== 0 && (
+        <Table withRowBorders={false}>
+          <Table.Thead>
+            {attributes.map((attribute) => (
+              <Table.Tr>
+                <Table.Td> {attribute.attribute}</Table.Td>
+                <Table.Td ta={"end"}>{attribute.type}</Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Thead>
+        </Table>
+      )}
+    </Box>
+  );
+};
 
 const ObjectGraph = () => {
-  const { data: o2o = [] } = useO2o();
+  const { data: o2o } = useO2o();
+  const { data: objectAttributes } = useObjectAttributes();
 
-  const graph: InputGraph = useMemo(
-    () => ({
-      edges: o2o.map(({ src, target }) => ({ from: src, to: target })),
-      nodes: Array.from(
-        new Set(
-          o2o.flatMap(({ target, src }) => [
-            { id: target, label: target },
-            { id: src, label: src },
-          ]),
-        ),
-      ),
-    }),
-    [o2o],
+  const nodes = useMemo(() => {
+    if (!o2o || !objectAttributes) return [];
+    const objectTypeNames = Array.from(
+      new Set(o2o.flatMap(({ target, src }) => [target, src])),
+    );
+    return objectTypeNames.map(
+      (objectName) =>
+        ({
+          id: objectName,
+          data: {
+            type: "rectangle",
+            inner: (
+              <AttributeTable
+                name={objectName}
+                attributes={objectAttributes[objectName]}
+              />
+            ),
+          },
+        }) satisfies NodeComponents,
+    );
+  }, [o2o, objectAttributes]);
+
+  return (
+    <Box w={"100%"} h={"100%"}>
+      {o2o && objectAttributes && (
+        <Graph
+          initialNodes={nodes}
+          initialEdges={o2o.map(({ src, target }) => ({
+            source: src,
+            target,
+          }))}
+        />
+      )}
+    </Box>
   );
-  return <FloatingFlow graph={graph} />;
 };
 
 export default ObjectGraph;
