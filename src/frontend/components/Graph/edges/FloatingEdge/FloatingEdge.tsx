24c6c6045211b09700d2042def96cdb75547608a
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-09 19:25:06 +0200

    :sparkles: Add new marker ends to Floating Edge

--- src/frontend/components/Graph/edges/FloatingEdge/FloatingEdge.tsx
+++ src/frontend/components/Graph/edges/FloatingEdge/FloatingEdge.tsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import {
   BaseEdge,
   Edge,
+  EdgeLabelRenderer,
   EdgeProps,
   getStraightPath,
   useInternalNode,
 } from "@xyflow/react";
 import { getEdgeParams } from "@/components/Graph/util/getEdgeParams";
 
-export type FloatingEdgeType = Edge<{}, "floating">;
+export type FloatingEdgeType = Edge<
+  { start?: ReactNode; end?: ReactNode },
+  "floating"
+>;
+
+const EdgeLabel: React.FC<{
+  transform: string;
+  children: ReactNode;
+}> = ({ transform, children }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        fontSize: 8,
+        fontWeight: 400,
+        transform,
+      }}
+      className="nodrag nopan"
+    >
+      {children}
+    </div>
+  );
+};
+
+const customMarkes = ["url('#double-rect')", "url('#rect')"];
 
 const FloatingEdge = ({
-  id,
+  data,
   source,
   target,
   markerEnd,
-  style,
+  ...props
 }: EdgeProps<FloatingEdgeType>) => {
   const sourceNode = useInternalNode(source);
   const targetNode = useInternalNode(target);
@@ -33,7 +58,77 @@ const FloatingEdge = ({
 
   return (
     <>
-      <BaseEdge id={id} style={style} path={edgePath} markerEnd={markerEnd} />
+      <svg style={{ position: "absolute", top: 0, left: 0 }}>
+        <defs>
+          <marker
+            id="rect-end"
+            markerWidth="8"
+            markerHeight="8"
+            refX="8"
+            refY="5"
+            orient="auto"
+          >
+            <rect x="2" y="2" width="6" height="6" fill="black" />
+          </marker>
+          <marker
+            id="double-rect-end"
+            markerWidth="14"
+            markerHeight="10"
+            refX="12" // ← adjust to move marker back or forward
+            refY="5"
+            orient="auto"
+          >
+            <rect x="2" y="2" width="4" height="6" fill="black" />
+            <rect x="8" y="2" width="4" height="6" fill="black" />
+          </marker>
+          <marker
+            id="rect"
+            markerWidth="8"
+            markerHeight="8"
+            refX="3"
+            refY="5"
+            orient="auto"
+          >
+            <rect x="2" y="2" width="6" height="6" fill="black" />
+          </marker>
+          <marker
+            id="double-rect"
+            markerWidth="14"
+            markerHeight="10"
+            refX="2" // ← adjust to move marker back or forward
+            refY="5"
+            orient="auto"
+          >
+            <rect x="2" y="2" width="4" height="6" fill="black" />
+            <rect x="8" y="2" width="4" height="6" fill="black" />
+          </marker>
+        </defs>
+      </svg>
+      <BaseEdge
+        path={edgePath}
+        markerEnd={
+          customMarkes.includes(markerEnd ?? "")
+            ? markerEnd?.replace("rect", "rect-end")
+            : markerEnd
+        }
+        {...props}
+      />
+      <EdgeLabelRenderer>
+        {data?.start && (
+          <EdgeLabel
+            transform={`translate(-40%, 0%) translate(${sx}px,${sy}px)`}
+          >
+            {data?.start}
+          </EdgeLabel>
+        )}
+        {data?.end && (
+          <EdgeLabel
+            transform={`translate(-40%, 0%) translate(${tx}px,${ty}px)`}
+          >
+            {data?.end}
+          </EdgeLabel>
+        )}
+      </EdgeLabelRenderer>
     </>
   );
 };
