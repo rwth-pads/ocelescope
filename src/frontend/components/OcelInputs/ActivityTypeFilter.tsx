a5211823d259e7ed804441999210b1beddac8833
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-17 13:41:27 +0200

    :sparkles: Make ActivityCount optional

--- src/frontend/components/OcelInputs/ActivityTypeFilter.tsx
+++ src/frontend/components/OcelInputs/ActivityTypeFilter.tsx
@@ -15,12 +15,14 @@ type EventTypeFilterInputProps = {
   value: string[];
   onChange: (val: string[]) => void;
   exclude?: boolean;
+  showGraph?: boolean;
 };
 
 const EventTypeFilterInput: React.FC<EventTypeFilterInputProps> = ({
   exclude = false,
   value,
   onChange,
+  showGraph,
 }) => {
   const { data: eventTypes = {}, isLoading } = useEventCounts();
   const colorMap = assignUniqueColors(Object.keys(eventTypes));
@@ -30,40 +32,42 @@ const EventTypeFilterInput: React.FC<EventTypeFilterInputProps> = ({
         <LoadingOverlay />
       ) : (
         <>
-          <BarChart
-            h={40 * Object.keys(eventTypes).length}
-            data={Object.entries(eventTypes).map(([name, count]) => ({
-              name,
-              count,
-              color:
-                exclude !== value.includes(name) ? colorMap[name] : "gray.6",
-            }))}
-            minBarSize={30}
-            tooltipProps={{
-              content: ({ label }) => (
-                <Paper px="md" py="sm" withBorder shadow="md" radius="md">
-                  <Text>{label}</Text>
-                </Paper>
-              ),
-            }}
-            dataKey="name"
-            orientation="vertical"
-            yAxisProps={{ width: 130 }}
-            series={[{ name: "count", color: "gray.6" }]}
-            gridAxis="none"
-            barChartProps={{
-              onClick: ({ activeLabel }) => {
-                if (!activeLabel) return;
-                if (exclude && value.includes(activeLabel)) {
-                  onChange(value.filter((v) => v !== activeLabel));
-                }
+          {showGraph && (
+            <BarChart
+              h={40 * Object.keys(eventTypes).length}
+              data={Object.entries(eventTypes).map(([name, count]) => ({
+                name,
+                count,
+                color:
+                  exclude !== value.includes(name) ? colorMap[name] : "gray.6",
+              }))}
+              minBarSize={30}
+              tooltipProps={{
+                content: ({ label }) => (
+                  <Paper px="md" py="sm" withBorder shadow="md" radius="md">
+                    <Text>{label}</Text>
+                  </Paper>
+                ),
+              }}
+              dataKey="name"
+              orientation="vertical"
+              yAxisProps={{ width: 130 }}
+              series={[{ name: "count", color: "gray.6" }]}
+              gridAxis="none"
+              barChartProps={{
+                onClick: ({ activeLabel }) => {
+                  if (!activeLabel) return;
+                  if (exclude && value.includes(activeLabel)) {
+                    onChange(value.filter((v) => v !== activeLabel));
+                  }
 
-                if (!value.includes(activeLabel)) {
-                  onChange([...value, activeLabel]);
-                }
-              },
-            }}
-          />
+                  if (!value.includes(activeLabel)) {
+                    onChange([...value, activeLabel]);
+                  }
+                },
+              }}
+            />
+          )}
           <MultiSelect
             label="Event Types"
             data={eventTypes ? Object.keys(eventTypes) : []}
