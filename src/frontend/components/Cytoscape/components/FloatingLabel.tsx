748372b20a3739766b2fc40fbac864a2f05f2860
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-18 08:32:46 +0200

    :sparkles: Floating labels

--- src/frontend/components/Cytoscape/components/FloatingLabel.tsx
+++ src/frontend/components/Cytoscape/components/FloatingLabel.tsx
@@ -2,37 +2,97 @@ import { Affix, Paper } from "@mantine/core";
 import { useClickOutside } from "@mantine/hooks";
 import { useCytoscapeContext } from "../CytoscapeContext";
 import { useEffect, useState } from "react";
+import { EventObject } from "cytoscape";
+import { EventObjectNode } from "cytoscape";
 
 type TriggerType = {
-  action: "hover" | "rightClick" | "leftClick";
+  action: "hover" | "rightClick" | "leftClick" | "doubleClick";
   target?: "node" | "edge";
 };
 
+const triggerActionToStartEvent = {
+  hover: "tapdragover",
+  rightClick: "tap",
+  leftClick: "cxttap",
+  doubleClick: "dbltap",
+};
+
+const triggerActionToEndEvent = {
+  hover: "tapdragout",
+  rightClick: undefined,
+  leftClick: undefined,
+  doubleClick: undefined,
+};
+
 const FloatingAnotation: React.FC<{
-  children: React.ReactNode;
-  triggerType: TriggerType;
-}> = ({ children }) => {
+  content: (event: EventObject) => React.ReactNode;
+  trigger: TriggerType;
+}> = ({ content, trigger }) => {
   const { cy } = useCytoscapeContext();
 
-  const [position, setPositon] = useState<{ x: number; y: number } | undefined>(
-    undefined,
-  );
+  const [position, setPositon] = useState<
+    { x: number; y: number; event: EventObject } | undefined
+  >(undefined);
   const ref = useClickOutside(() => setPositon(undefined));
 
   useEffect(() => {
     if (!cy.current) return;
     const cytoscape = cy.current;
-    cytoscape.addListener("tapdragover", "node", (event) =>
-      setPositon({ x: event.originalEvent.x, y: event.originalEvent.y }),
-    );
-    cytoscape.addListener("tapdragout", "node", (event) => {
-      setPositon(undefined);
-    });
+
+    const triggerEndEvent = triggerActionToEndEvent[trigger.action];
+    if (trigger.target) {
+      cytoscape.addListener(
+        triggerActionToStartEvent[trigger.action],
+        trigger.target,
+        (event) =>
+          setPositon({
+            x: event.originalEvent.x,
+            y: event.originalEvent.y,
+            event,
+          }),
+      );
+
+      if (triggerEndEvent) {
+        cytoscape.addListener(triggerEndEvent, trigger.target, (event) => {
+          setPositon(undefined);
+        });
+      }
+    } else {
+      cytoscape.addListener(
+        triggerActionToStartEvent[trigger.action],
+        (event) =>
+          setPositon({
+            x: event.originalEvent.x,
+            y: event.originalEvent.y,
+            event,
+          }),
+      );
+
+      if (triggerEndEvent) {
+        cytoscape.addListener(triggerEndEvent, (event) => {
+          setPositon(undefined);
+        });
+      }
+    }
+
     return () => {
-      cytoscape.removeListener("tapdragout", "node");
-      cytoscape.removeListener("tapdragover", "node");
+      if (trigger.target) {
+        cytoscape.removeListener(
+          triggerActionToStartEvent[trigger.action],
+          trigger.target,
+        );
+
+        if (triggerEndEvent) {
+          cytoscape.removeListener(triggerEndEvent, trigger.target);
+        }
+      }
+      cytoscape.removeListener(triggerActionToStartEvent[trigger.action]);
+
+      if (triggerEndEvent) {
+        cytoscape.removeListener(triggerEndEvent);
+      }
     };
-  }, [cy]);
+  }, [cy, trigger]);
 
   if (!position) {
     return null;
@@ -40,7 +100,9 @@ const FloatingAnotation: React.FC<{
 
   return (
     <Affix ref={ref} position={{ top: position.y, left: position.x }}>
-      <Paper w={200} h={200} shadow="xs" p="xl"></Paper>
+      <Paper w={200} h={200} shadow="xs" p="xl">
+        {content(position.event)}
+      </Paper>
     </Affix>
   );
 };
