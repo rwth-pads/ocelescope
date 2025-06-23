8697229eb4b04ede6e27ebec4c078d0e8f569afe
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-23 13:17:29 +0200

    :poop: Ahhhhhhhh help me

--- src/frontend/components/Filters/FilterComponents/RelationFilter.tsx
+++ src/frontend/components/Filters/FilterComponents/RelationFilter.tsx
@@ -1,15 +1,8 @@
-import { useO2e, useO2o } from "@/api/fastapi/info/info";
+import { useO2e } from "@/api/fastapi/info/info";
 import { ConfigByType } from "../types";
-import {
-  Box,
-  Grid,
-  Group,
-  LoadingOverlay,
-  RangeSlider,
-  Select,
-} from "@mantine/core";
-import { useMemo } from "react";
-import { ArrowRight, ArrowRightFromLine } from "lucide-react";
+import { Box, Grid, LoadingOverlay, RangeSlider, Select } from "@mantine/core";
+import { memo, useMemo } from "react";
+import { ArrowRight } from "lucide-react";
 import { OcelInputType } from "@/types/ocel";
 import { E2OCountFilterConfigMode } from "@/api/fastapi-schemas";
 
@@ -17,123 +10,121 @@ type RelationFilterProps = {
   value: ConfigByType<"e2o_count">;
   onChange: (newValue: ConfigByType<"e2o_count">) => void;
 } & OcelInputType;
-const RelationFilter: React.FC<RelationFilterProps> = ({
-  value,
-  onChange,
-  ...ocelParams
-}) => {
-  const { data: e2o, isLoading } = useO2e({ ...ocelParams });
+const RelationFilter: React.FC<RelationFilterProps> = memo(
+  ({ value, onChange, ...ocelParams }) => {
+    const { data: e2o, isLoading } = useO2e({ ...ocelParams });
 
-  const relevantRelations = useMemo(() => {
-    return e2o?.filter(({ max_count, min_count }) => min_count < max_count);
-  }, [e2o]);
+    const relevantRelations = useMemo(() => {
+      return e2o?.filter(({ max_count, min_count }) => min_count < max_count);
+    }, [e2o]);
 
-  const { objectTypeNames, activitNames, min, max } = useMemo(() => {
-    if (!e2o) {
-      return {
-        activitNames: [],
-        objectTypeNames: [],
-        filteredRelations: [],
-        min: 0,
-        max: 0,
-      };
-    }
-    const filteredRelations = e2o.filter(
-      ({ max_count, min_count, activity, object_type }) =>
-        min_count < max_count &&
-        (value.event_type === "" || value.event_type === activity) &&
-        (value.object_type === "" || value.object_type === object_type),
-    );
+    const { objectTypeNames, activitNames, min, max } = useMemo(() => {
+      if (!e2o) {
+        return {
+          activitNames: [],
+          objectTypeNames: [],
+          filteredRelations: [],
+          min: 0,
+          max: 0,
+        };
+      }
+      const filteredRelations = e2o.filter(
+        ({ max_count, min_count, activity, object_type }) =>
+          min_count < max_count &&
+          (value.event_type === "" || value.event_type === activity) &&
+          (value.object_type === "" || value.object_type === object_type),
+      );
 
-    const activitNames = Array.from(
-      new Set(filteredRelations.map(({ activity }) => activity)),
-    );
+      const activitNames = Array.from(
+        new Set(filteredRelations.map(({ activity }) => activity)),
+      );
 
-    const objectTypeNames = Array.from(
-      new Set(filteredRelations.map(({ object_type }) => object_type)),
-    );
+      const objectTypeNames = Array.from(
+        new Set(filteredRelations.map(({ object_type }) => object_type)),
+      );
 
-    const min = Math.min(
-      ...filteredRelations.map(({ min_count }) => min_count),
-    );
+      const min = Math.min(
+        ...filteredRelations.map(({ min_count }) => min_count),
+      );
 
-    const max = Math.max(
-      ...filteredRelations.map(({ max_count }) => max_count),
-    );
+      const max = Math.max(
+        ...filteredRelations.map(({ max_count }) => max_count),
+      );
 
-    return { filteredRelations, activitNames, objectTypeNames, min, max };
-  }, [e2o, value]);
+      return { filteredRelations, activitNames, objectTypeNames, min, max };
+    }, [e2o, value]);
 
-  return (
-    <Box w={"100%"} h={"100%"} pos={"relative"}>
-      <LoadingOverlay visible={isLoading} />
-      <Grid align="center" gutter={"md"}>
-        <Grid.Col span={3} offset={9}>
-          <Select
-            data={[
-              { value: "include", label: "Include" },
-              { value: "exlude", label: "Exlude" },
-            ]}
-            allowDeselect={false}
-            label={"Mode"}
-            value={value.mode ?? "include"}
-            onChange={(newMode) =>
+    return (
+      <Box w={"100%"} h={"100%"} pos={"relative"}>
+        <LoadingOverlay visible={isLoading} />
+        <Grid align="center" gutter={"md"}>
+          <Grid.Col span={3} offset={9}>
+            <Select
+              data={[
+                { value: "include", label: "Include" },
+                { value: "exlude", label: "Exlude" },
+              ]}
+              allowDeselect={false}
+              label={"Mode"}
+              value={value.mode ?? "include"}
+              onChange={(newMode) =>
+                onChange({
+                  ...value,
+                  mode: (newMode as E2OCountFilterConfigMode) ?? undefined,
+                })
+              }
+            ></Select>
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Select
+              label={"Activity Type"}
+              value={value.event_type ?? undefined}
+              data={activitNames}
+              allowDeselect
+              onChange={(newActivity) =>
+                onChange({ ...value, event_type: newActivity ?? "" })
+              }
+            />
+          </Grid.Col>
+          <Grid.Col
+            span={4}
+            display={"flex"}
+            style={{ justifyContent: "center" }}
+          >
+            <ArrowRight height={36} />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <Select
+              label={"Object Type"}
+              value={value.object_type ?? undefined}
+              data={objectTypeNames}
+              allowDeselect
+              onChange={(newObjectType) =>
+                onChange({ ...value, object_type: newObjectType ?? "" })
+              }
+            />
+          </Grid.Col>
+        </Grid>
+
+        {value.event_type !== "" && value.object_type !== "" && (
+          <RangeSlider
+            py={"md"}
+            minRange={0}
+            value={[value.min ?? min, value.max ?? max]}
+            max={max}
+            min={min}
+            onChange={(newRange) =>
               onChange({
                 ...value,
-                mode: (newMode as E2OCountFilterConfigMode) ?? undefined,
+                min: newRange[0],
+                max: newRange[1],
               })
             }
-          ></Select>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Select
-            label={"Activity Type"}
-            value={value.event_type ?? undefined}
-            data={activitNames}
-            allowDeselect
-            onChange={(newActivity) =>
-              onChange({ ...value, event_type: newActivity ?? "" })
-            }
           />
-        </Grid.Col>
-        <Grid.Col
-          span={4}
-          display={"flex"}
-          style={{ justifyContent: "center" }}
-        >
-          <ArrowRight height={36} />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Select
-            label={"Object Type"}
-            value={value.object_type ?? undefined}
-            data={objectTypeNames}
-            allowDeselect
-            onChange={(newObjectType) =>
-              onChange({ ...value, object_type: newObjectType ?? "" })
-            }
-          />
-        </Grid.Col>
-      </Grid>
-
-      {value.event_type !== "" && value.object_type !== "" && (
-        <RangeSlider
-          py={"md"}
-          minRange={0}
-          value={[value.min ?? min, value.max ?? max]}
-          max={max}
-          min={min}
-          onChange={(newRange) =>
-            onChange({
-              ...value,
-              min: newRange[0],
-              max: newRange[1],
-            })
-          }
-        />
-      )}
-    </Box>
-  );
-};
+        )}
+      </Box>
+    );
+  },
+);
 
 export default RelationFilter;
--- src/frontend/components/Filters/FilterComponents/TimeFrameFilter.tsx
+++ src/frontend/components/Filters/FilterComponents/TimeFrameFilter.tsx
@@ -1,12 +1,14 @@
 import { useEventCounts, useTimeInfo } from "@/api/fastapi/info/info";
 import { Box, Grid, LoadingOverlay, RangeSlider } from "@mantine/core";
-import { useMemo } from "react";
+import { memo, useMemo } from "react";
 import { ConfigByType, FilterType } from "../types";
 import assignUniqueColors from "@/util/colors";
 import { BarChart, BarChartSeries } from "@mantine/charts";
 import { DateTimePicker } from "@mantine/dates";
 import dayjs from "dayjs";
 import { OcelInputType } from "@/types/ocel";
+import { EntityTimeInfo } from "@/api/fastapi-schemas";
+import { useDebouncedValue } from "@mantine/hooks";
 
 type TimeFrameFilterProps<K extends FilterType> = {
   value: ConfigByType<K>;
@@ -14,101 +16,157 @@ type TimeFrameFilterProps<K extends FilterType> = {
   showGraph?: boolean;
 } & OcelInputType;
 
-const TimeFrameFilter: React.FC<TimeFrameFilterProps<"time_frame">> = ({
-  value,
-  onChange,
-  showGraph = true,
-  ocel_id,
-  ocel_version,
-}) => {
-  const { data: timeInfo, isLoading } = useTimeInfo({ ocel_id, ocel_version });
-  const { data: eventCount } = useEventCounts({ ocel_id, ocel_version });
+const TimeGraph: React.FC<{
+  timeInfo: EntityTimeInfo;
+  startDate: string;
+  endDate: string;
+}> = memo(({ timeInfo, startDate, endDate }) => {
+  const data = useMemo(() => {
+    const data = timeInfo.date_distribution.map(({ date, entity_count }) => {
+      const d = dayjs(date);
+      const start = dayjs(startDate);
+      const end = dayjs(endDate);
 
-  const { data, series } = useMemo(() => {
-    if (!timeInfo || !eventCount) {
-      return { data: [], series: [] };
-    }
+      const isInRange =
+        d.isSame(start) ||
+        d.isSame(end) ||
+        (d.isAfter(start) && d.isBefore(end));
 
-    if (Object.keys(eventCount ?? {}).length <= 20) {
-      const colorMap = assignUniqueColors(Object.keys(eventCount ?? {}));
-
-      const data = timeInfo.date_distribution.map(({ date, entity_count }) => ({
+      return {
         date,
-        ...entity_count,
-      }));
+        ...(isInRange
+          ? {
+              value: Object.values(entity_count).reduce(
+                (acc, curr) => acc + curr,
+                0,
+              ),
+            }
+          : {
+              disabledValue: Object.values(entity_count).reduce(
+                (acc, curr) => acc + curr,
+                0,
+              ),
+            }),
+      };
+    });
 
-      const series: BarChartSeries[] = Object.entries(colorMap).map(
-        ([entity_type, color]) => ({ name: entity_type, color: color }),
-      );
-      return { data, series };
-    }
-
-    const data = timeInfo.date_distribution.map(({ date, entity_count }) => ({
-      date,
-      value: Object.values(entity_count).reduce((acc, curr) => acc + curr, 0),
-    }));
-
-    const series: BarChartSeries[] = [{ name: "value", color: "blue" }];
-    return { data, series };
-  }, [timeInfo, eventCount]);
-
+    return data;
+  }, [timeInfo, startDate, endDate]);
   return (
-    <Box pos={"relative"} w={"100%"} h={"100%"}>
-      <LoadingOverlay visible={isLoading} />
-      {timeInfo && (
-        <Grid justify="center" align="center">
-          <Grid.Col span={8} />
-          <Grid.Col span={4}></Grid.Col>
-          {showGraph && (
-            <>
-              <Grid.Col span={12}>
-                <BarChart
-                  h={300}
-                  w={"100%"}
-                  data={data}
-                  dataKey="date"
-                  type="stacked"
-                  series={series}
-                  withYAxis={false}
-                  withXAxis={false}
-                  barChartProps={{ barCategoryGap: 0, barGap: 0 }}
-                />
-              </Grid.Col>
-            </>
-          )}
-          <Grid.Col span={2}>
-            <DateTimePicker
-              minDate={timeInfo.start_time}
-              maxDate={value.end_time ?? timeInfo.end_time}
-              value={value.start_time ?? timeInfo.start_time}
-              onChange={(newStartDate) =>
-                onChange({
-                  ...value,
-                  start_time: dayjs(newStartDate).toISOString(),
-                })
-              }
-            />
-          </Grid.Col>
-          <Grid.Col span={8}>
-            <RangeSlider minRange={0} min={1} max={data.length} />
-          </Grid.Col>
-          <Grid.Col span={2}>
-            <DateTimePicker
-              minDate={value.start_time ?? timeInfo.start_time}
-              maxDate={timeInfo.end_time}
-              value={value.end_time ?? timeInfo.end_time}
-              onChange={(newEndDate) =>
-                onChange({
-                  ...value,
-                  end_time: dayjs(newEndDate).toISOString(),
-                })
-              }
-            />
-          </Grid.Col>
-        </Grid>
-      )}
-    </Box>
+    <Grid.Col span={12}>
+      <BarChart
+        h={300}
+        w={"100%"}
+        data={data}
+        dataKey="date"
+        type="stacked"
+        series={[
+          { name: "value", color: "blue" },
+          { name: "disabledValue", color: "red" },
+        ]}
+        withYAxis={false}
+        withXAxis={false}
+        barChartProps={{ barCategoryGap: 0, barGap: 0 }}
+      />
+    </Grid.Col>
   );
-};
+});
+
+const TimeFrameFilter: React.FC<TimeFrameFilterProps<"time_frame">> = memo(
+  ({ value, onChange, showGraph = true, ocel_id, ocel_version }) => {
+    const { data: timeInfo, isLoading } = useTimeInfo({
+      ocel_id,
+      ocel_version,
+    });
+    const { data: eventCount } = useEventCounts({ ocel_id, ocel_version });
+
+    const { amountOfDays } = useMemo(() => {
+      if (!timeInfo || !eventCount) {
+        return { amountOfDays: 0 };
+      }
+
+      const startTime = dayjs(timeInfo.start_time);
+      const endTime = dayjs(timeInfo.end_time);
+
+      const amountOfDays = endTime.diff(startTime, "day");
+
+      return { amountOfDays };
+    }, [timeInfo, eventCount]);
+
+    const sliderValue: [number, number] = useMemo(() => {
+      if (!timeInfo) return [0, 0];
+
+      const startDiff = value.start_time
+        ? dayjs(value.start_time).diff(dayjs(timeInfo.start_time), "day")
+        : 0;
+
+      const endDiff = value.end_time
+        ? dayjs(value.end_time).diff(dayjs(timeInfo.start_time), "day")
+        : amountOfDays;
+
+      return [startDiff, endDiff];
+    }, [value, timeInfo, amountOfDays]);
+
+    return (
+      <Box pos={"relative"} w={"100%"} h={"100%"}>
+        <LoadingOverlay visible={isLoading} />
+        {timeInfo && (
+          <Grid justify="center" align="center">
+            <Grid.Col span={3}>
+              <DateTimePicker
+                minDate={timeInfo.start_time}
+                maxDate={value.end_time ?? timeInfo.end_time}
+                value={value.start_time ?? timeInfo.start_time}
+                onChange={(newStartDate) =>
+                  onChange({
+                    ...value,
+                    start_time: dayjs(newStartDate).toISOString(),
+                  })
+                }
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <RangeSlider
+                minRange={0}
+                min={0}
+                max={amountOfDays}
+                value={sliderValue}
+                label={(value) => {
+                  return dayjs(timeInfo.start_time)
+                    .add(value, "day")
+                    .format("YYYY-MM-DD");
+                }}
+                onChange={([startDiff, endDiff]) => {
+                  onChange({
+                    ...value,
+                    start_time: dayjs(timeInfo.start_time)
+                      .add(startDiff, "days")
+                      .toISOString(),
+                    end_time: dayjs(timeInfo.start_time)
+                      .add(endDiff, "days")
+                      .toISOString(),
+                  });
+                }}
+              />
+            </Grid.Col>
+            <Grid.Col span={3}>
+              <DateTimePicker
+                minDate={value.start_time ?? timeInfo.start_time}
+                maxDate={timeInfo.end_time}
+                value={value.end_time ?? timeInfo.end_time}
+                onChange={(newEndDate) =>
+                  onChange({
+                    ...value,
+                    end_time: dayjs(newEndDate).toISOString(),
+                  })
+                }
+              />
+            </Grid.Col>
+          </Grid>
+        )}
+      </Box>
+    );
+  },
+);
 
 export default TimeFrameFilter;
