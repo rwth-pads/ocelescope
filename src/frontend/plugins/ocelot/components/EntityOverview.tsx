6d4c92a2e7dbb3d683869d069cbd3e035994638c
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-06-30 13:46:21 +0200

    :sparkles: EntityOverview

---
+++ src/frontend/plugins/ocelot/components/EntityOverview.tsx
@@ -0,0 +1,68 @@
+import {
+  EventAttributes200,
+  EventCounts200,
+  RelationCountSummary,
+} from "@/api/fastapi-schemas";
+import { Input, SimpleGrid, Stack } from "@mantine/core";
+import { useDebouncedState } from "@mantine/hooks";
+import { SearchIcon } from "lucide-react";
+import { useMemo } from "react";
+import EntityCard from "./EntityCard";
+
+type EntityOverviewProps = {
+  entityCounts: EventCounts200;
+  relations: RelationCountSummary[];
+  attributes: EventAttributes200;
+  search?: string;
+};
+
+const EntityOverview: React.FC<EntityOverviewProps> = ({
+  attributes,
+  relations,
+  entityCounts,
+  search = "",
+}) => {
+  const relationMap = useMemo(() => {
+    return relations.reduce<Record<string, RelationCountSummary[]>>(
+      (relationMap, currentRelation) => {
+        if (currentRelation.source in relationMap)
+          relationMap[currentRelation.source].push(currentRelation);
+        else relationMap[currentRelation.source] = [];
+        return relationMap;
+      },
+      {},
+    );
+  }, [relations]);
+
+  const filteredEvents = useMemo(() => {
+    const toSearch = search.toLowerCase();
+    return Object.entries(entityCounts).filter(
+      ([event, _]) =>
+        event.toLowerCase().includes(search.toLowerCase()) ||
+        relationMap[event]?.some(
+          ({ target, qualifier }) =>
+            target.toLowerCase().includes(toSearch) ||
+            qualifier.toLowerCase().includes(toSearch),
+        ) ||
+        attributes[event]?.some(({ attribute }) =>
+          attribute.toLowerCase().includes(toSearch),
+        ),
+    );
+  }, [search, relationMap, entityCounts, attributes]);
+
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, lg: 5 }}>
+      {filteredEvents.map(([name, count]) => (
+        <EntityCard
+          key={name}
+          count={count}
+          name={name}
+          attributeSummaries={attributes[name]}
+          relationSummaries={relationMap[name]}
+        />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default EntityOverview;
--- src/frontend/plugins/ocelot/pages/eventOverview.tsx
+++ src/frontend/plugins/ocelot/pages/eventOverview.tsx
@@ -10,45 +10,15 @@ import { useMemo } from "react";
 import { RelationCountSummary } from "@/api/fastapi-schemas";
 import { SearchIcon } from "lucide-react";
 import { useDebouncedState } from "@mantine/hooks";
+import EntityOverview from "../components/EntityOverview";
 
 const EventOverview = () => {
   const { data: eventsAttributes = {} } = useEventAttributes();
-  const { data: e2o } = useE2o();
+  const { data: e2o = [] } = useE2o();
   const { data: eventCounts, isLoading: isEventCountsLoading } =
     useEventCounts();
 
   const [searchValue, setSearchValue] = useDebouncedState("", 200);
-
-  const relationMap = useMemo(() => {
-    if (!e2o) return {};
-
-    return e2o.reduce<Record<string, RelationCountSummary[]>>(
-      (relationMap, currentRelation) => {
-        if (currentRelation.source in relationMap)
-          relationMap[currentRelation.source].push(currentRelation);
-        else relationMap[currentRelation.source] = [];
-        return relationMap;
-      },
-      {},
-    );
-  }, [e2o]);
-
-  const filteredEvents = useMemo(() => {
-    const toSearch = searchValue.toLowerCase();
-    return Object.entries(eventCounts ?? {}).filter(
-      ([event, _]) =>
-        event.toLowerCase().includes(searchValue.toLowerCase()) ||
-        relationMap[event].some(
-          ({ target, qualifier }) =>
-            target.toLowerCase().includes(toSearch) ||
-            qualifier.toLowerCase().includes(toSearch),
-        ) ||
-        eventsAttributes[event].some(({ attribute }) =>
-          attribute.toLowerCase().includes(toSearch),
-        ),
-    );
-  }, [searchValue, relationMap, eventCounts, eventsAttributes]);
-
   return (
     <>
       <LoadingOverlay visible={isEventCountsLoading} />
@@ -59,17 +29,12 @@ const EventOverview = () => {
           onChange={(newSearch) => setSearchValue(newSearch.target.value)}
         />
         {eventCounts && (
-          <SimpleGrid cols={{ base: 1, sm: 2, lg: 5 }}>
-            {filteredEvents.map(([name, count]) => (
-              <EntityCard
-                key={name}
-                count={count}
-                name={name}
-                attributeSummaries={eventsAttributes[name]}
-                relationSummaries={relationMap[name]}
-              />
-            ))}
-          </SimpleGrid>
+          <EntityOverview
+            relations={e2o}
+            entityCounts={eventCounts}
+            attributes={eventsAttributes}
+            search={searchValue}
+          />
         )}
       </Stack>
     </>
