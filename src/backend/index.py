98eef6d881dfa3ed3bb52d1be8eab3e89aea142d
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-07-19 22:55:08 +0200

    :truck: Rename plugin_loader to regstrar.py

--- src/backend/index.py
+++ src/backend/index.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 from api.config import OceanConfig
 from api.docs import init_custom_docs
@@ -14,12 +15,13 @@ from api.utils import (
 from ocel.default_ocel import (
     load_default_ocels,
 )
-from plugin_loader import register_extensions, register_plugins
+from registrar import register_extensions, register_plugins
 
 from fastapi import FastAPI
 from routes import routes
 from util.misc import export_example_settings_as_dotenv
 from version import __version__
+from api.logger import LOGGER_CONFIG
 
 """
 In this file, all API routes of the OCEAn application are defined.
@@ -67,8 +69,16 @@ def post_init_tasks():
     # Generate .env.example file
     export_example_settings_as_dotenv(OceanConfig, ".env.example")
 
+    export_openapi_schema(app, "../frontend/schemas/mainBackendOpenApi.json")
+
 
 post_init_tasks()
 
 if __name__ == "__main__":
-    export_openapi_schema(app, "../frontend/schemas/mainBackendOpenApi.json")
+    uvicorn.run(
+        "index:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=False,
+        log_config=LOGGER_CONFIG,
+    )
