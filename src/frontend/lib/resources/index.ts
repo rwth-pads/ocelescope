08e7d5c9a3882fd4d8973cebbdfe9a167d9b9735
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-07-25 17:04:40 +0200

    :fire: Remove gitmoji artifacts

--- src/frontend/lib/resources/index.ts
+++
@@ -1,5 +0,0 @@
-import { ResourceType, ResourceViewDefinition } from "@/types/resources";
-
-export const defineResourceView = <K extends ResourceType>(
-  def: ResourceViewDefinition<K>,
-) => def;
