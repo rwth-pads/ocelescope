c3fc1da187e6b1058ae6387ed06d3ed96d5a4e4d
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-11-02 12:51:34 +0100

    :sparkles: Scramble unique colors more

--- src/frontend/util/colors.ts
+++ src/frontend/util/colors.ts
@@ -3,10 +3,10 @@ const assignUniqueColors = (strings: string[]) => {
   const uniqueStrings = Array.from(new Set(strings));
   const colorMap: Record<string, string> = {};
 
-  uniqueStrings.forEach((str, index) => {
-    const hue = (index * 360) / numStrings; // even distribution
-    const saturation = 70; // percent
-    const lightness = 50; // percent (not too dark/light)
+  uniqueStrings.sort().forEach((str, index) => {
+    const hue = (index * 360) / numStrings;
+    const saturation = 70;
+    const lightness = 50;
     const hsl = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
     colorMap[str] = hsl;
   });
