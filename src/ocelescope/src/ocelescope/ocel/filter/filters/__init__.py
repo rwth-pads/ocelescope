ddca2d0573faff8e1d8e8d860073ccca4511f618
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-11-22 15:10:33 +0100

    :fire: Remove old FilterType

--- src/ocelescope/src/ocelescope/ocel/filter/filters/__init__.py
+++ src/ocelescope/src/ocelescope/ocel/filter/filters/__init__.py
@@ -1,6 +1,3 @@
-from typing import TypedDict
-
-
 from ocelescope.ocel.filter.filters.attribute import EventAttributeFilter, ObjectAttributeFilter
 from ocelescope.ocel.filter.filters.entity_type import (
     EventTypeFilter,
@@ -9,19 +6,7 @@ from ocelescope.ocel.filter.filters.entity_type import (
 from ocelescope.ocel.filter.filters.relation_count import E2OCountFilter, O2OCountFilter
 from ocelescope.ocel.filter.filters.time_range import TimeFrameFilter
 
-
-class OCELFilter(TypedDict, total=False):
-    object_types: ObjectTypeFilter
-    event_type: EventTypeFilter
-    time_range: TimeFrameFilter
-    o2o_count: list[O2OCountFilter]
-    e2o_count: list[E2OCountFilter]
-    event_attributes: list[EventAttributeFilter]
-    object_attributes: list[ObjectAttributeFilter]
-
-
 __all__ = [
-    "OCELFilter",
     "ObjectTypeFilter",
     "EventTypeFilter",
     "ObjectAttributeFilter",
