cf0b378a2da89322f9efed90ab928a4daceec7c5
grkmr <goerkem@posteo.com>
2026-02-12 13:07:26 +0100

    :hammer: Css Modules

--- src/newFrontend/packages/core/tsdown.config.ts
+++ src/newFrontend/packages/core/tsdown.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "tsdown";
-
+import LightningCSS from "unplugin-lightningcss/rolldown";
+import injectCssPlugin from "@bosh-code/tsdown-plugin-inject-css";
 export default defineConfig({
   platform: "neutral",
   entry: ["src/index.ts", "src/styles/styles.ts"],
+  plugins: [
+    LightningCSS({
+      options: {
+        minify: true,
+      },
+    }),
+    injectCssPlugin(),
+  ],
 });
