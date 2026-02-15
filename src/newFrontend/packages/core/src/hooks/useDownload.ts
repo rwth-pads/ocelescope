19ee68959b4374def9deca2d36b94808ad0692bb
grkmr <goerkem@posteo.com>
2026-02-15 13:11:46 +0100

    :sparkles: Add useDownload

---
+++ src/newFrontend/packages/core/src/hooks/useDownload.ts
@@ -0,0 +1,37 @@
+import { saveAs } from "file-saver";
+import { env } from "../lib/env";
+import useSessionStore from "../store/sessionStore";
+
+export const useDownloadFile = () => {
+  const { sessionId } = useSessionStore();
+
+  const downloadFile = async (path: string) => {
+    try {
+      const response = await fetch(`${env.backend_url}${path}`, {
+        method: "GET",
+        headers: { [env.session_id]: sessionId ?? "" },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to download file");
+      }
+
+      const blob = await response.blob();
+      const contentDisposition = response.headers.get("Content-Disposition");
+      let filename = "downloaded-file";
+
+      if (contentDisposition) {
+        const match = contentDisposition.match(/filename="?([^"]+)"?/);
+        if (match?.[1]) {
+          filename = match[1];
+        }
+      }
+
+      saveAs(blob, filename);
+    } catch (err) {
+      console.error("Download error:", err);
+    }
+  };
+
+  return downloadFile;
+};
